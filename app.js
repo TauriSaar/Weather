76d32a56fef3dfadc4c68e4d7ca24fad75f7cfe2
Tauri <tauri.saar@voco.ee>
2022-10-27 09:03:18 +0300

    Fetched city weather data from API

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', cityweather)
- function weatherDataFetch(city) {
-   var key = '0c3dbc7c9707de0a34bae0310dd79aa5';
-   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-   .then(function(resp) {
-   return resp.json()
-   }) // Convert data to json
-    .then(function(data) {
-     console.log(data);
-   })
-    .catch(function() {
-     // catch any errors
-   });
+function weatherDataFetch(city) {
+    var key = '0c3dbc7c9707de0a34bae0310dd79aa5';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then(function(resp) {
+            return resp.json()
+        }) // Convert data to json
+        .then(function(data) {
+            console.log(data);
+            drawWeather(data);
+        })
+        .catch(function() {
+            // catch any errors
+        });
 }
 function cityweather(e) {
     weatherDataFetch('Tallinn');
 }
 
+function drawWeather(data) {
+    var celcius = Math.round(parseFloat(data.main.temp) -273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
